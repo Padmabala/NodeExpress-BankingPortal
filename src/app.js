de334577f1156dec767cf953416e86698802c1dc
samlin <samlin.wilson@accenture.com>
2019-11-26 18:03:56 +0530

    index.js is updated

--- src/app.js
+++ src/app.js
@@ -4,3 +4,9 @@ const app = require("express");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.length("/", (req, res) => {
+  res.render("index", { title: "Index" });
+});
+const server = app.listen(3000, () => {
+  console.log("Project is running on port :", server.address.port);
+});
