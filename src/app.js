4e17a94ac68415652f922c80bf94ef761160d6aa
kpadmabala@gmail.com <kpadmabala@gmail.com>
2019-11-26 21:43:28 +0530

    installed Express

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
-const fs = require("fs");
-const path = require("path");
-const app = require("express");
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-app.length("/", (req, res) => {
+const fs=require("fs");
+const express=require("express");
+const path=require("path");
+
+const app=express();
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+
+app.get("/", (req, res) => {
   res.render("index", { title: "Index" });
 });
 const server = app.listen(3000, () => {
-  console.log("Project is running on port :", server.address.port);
+  console.log("PS Project Running on port ", server.address.port,"!");
 });
+
+
