846509c930a7ce9981218fc83731db0257014769
kpadmabala@gmail.com <kpadmabala@gmail.com>
2019-11-27 06:07:46 +0530

    No big change

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const users=JSON.parse(userData);
 
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Account Summary" ,accounts:accounts});
+  res.render("index", { title: "Account Summary" ,account:accounts});
 });
 app.get('/savings',(req,res)=>{
   res.render("account",{account:accounts.savings});
