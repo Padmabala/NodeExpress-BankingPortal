ec4ee9114ca367f30da98c066efeac4041194870
kpadmabala@gmail.com <kpadmabala@gmail.com>
2019-11-27 06:00:09 +0530

    File handling and Routing

--- src/app.js
+++ src/app.js
@@ -7,12 +7,31 @@ app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
 
+const accountData =fs.readFileSync(path.join(__dirname,"json","accounts.json"),"UTF8");
+const accounts=JSON.parse(accountData);
+
+const userData=fs.readFileSync(path.join(__dirname,"json","users.json"),"UTF8");
+const users=JSON.parse(userData);
+
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Index" });
+  res.render("index", { title: "Account Summary" ,accounts:accounts});
 });
+app.get('/savings',(req,res)=>{
+  res.render("account",{account:accounts.savings});
+});
+app.get('/checking',(req,res)=>{
+  res.render('account',{account:accounts.checking});
+});
+app.get('/credit',(req,res)=>{
+  res.render('account',{account:accounts.credit});
+});
+app.get('/profile',(req,res)=>{
+  res.render("profile",{user:users[0]})
+});
+
 const server = app.listen(3000, () => {
-  console.log("PS Project Running on port ", server.address().port,"!");
+  console.log("PS Project Running on port 3000!");
 });
 
 
